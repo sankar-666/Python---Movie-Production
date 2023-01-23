697250f4c9e3868cf102ea5f72de5fbdaa66c2ed
sankar <sankusanku001@gmail.com>
2023-01-23 10:00:02 +0530

    files created

---
+++ db.sql
@@ -0,0 +1,159 @@
+/*
+SQLyog Ultimate v11.11 (64 bit)
+MySQL - 5.5.5-10.4.27-MariaDB : Database - movie_production
+*********************************************************************
+*/
+
+/*!40101 SET NAMES utf8 */;
+
+/*!40101 SET SQL_MODE=''*/;
+
+/*!40014 SET @OLD_UNIQUE_CHECKS=@@UNIQUE_CHECKS, UNIQUE_CHECKS=0 */;
+/*!40014 SET @OLD_FOREIGN_KEY_CHECKS=@@FOREIGN_KEY_CHECKS, FOREIGN_KEY_CHECKS=0 */;
+/*!40101 SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='NO_AUTO_VALUE_ON_ZERO' */;
+/*!40111 SET @OLD_SQL_NOTES=@@SQL_NOTES, SQL_NOTES=0 */;
+CREATE DATABASE /*!32312 IF NOT EXISTS*/`movie_production` /*!40100 DEFAULT CHARACTER SET utf8mb4 COLLATE utf8mb4_general_ci */;
+
+USE `movie_production`;
+
+/*Table structure for table `assigncrew` */
+
+DROP TABLE IF EXISTS `assigncrew`;
+
+CREATE TABLE `assigncrew` (
+  `assigncrew_id` int(11) NOT NULL AUTO_INCREMENT,
+  `filim_id` int(11) DEFAULT NULL,
+  `crew_id` int(11) DEFAULT NULL,
+  `date` varchar(100) DEFAULT NULL,
+  `status` varchar(100) DEFAULT NULL,
+  PRIMARY KEY (`assigncrew_id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+/*Data for the table `assigncrew` */
+
+/*Table structure for table `crew` */
+
+DROP TABLE IF EXISTS `crew`;
+
+CREATE TABLE `crew` (
+  `crew_id` int(11) NOT NULL AUTO_INCREMENT,
+  `login_id` int(11) DEFAULT NULL,
+  `fname` varchar(100) DEFAULT NULL,
+  `lname` varchar(100) DEFAULT NULL,
+  `place` varchar(100) DEFAULT NULL,
+  `phone` varchar(100) DEFAULT NULL,
+  `email` varchar(100) DEFAULT NULL,
+  PRIMARY KEY (`crew_id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+/*Data for the table `crew` */
+
+/*Table structure for table `filim` */
+
+DROP TABLE IF EXISTS `filim`;
+
+CREATE TABLE `filim` (
+  `filim_id` int(11) NOT NULL AUTO_INCREMENT,
+  `filim` varchar(100) DEFAULT NULL,
+  `details` varchar(100) DEFAULT NULL,
+  `desc` varchar(100) DEFAULT NULL,
+  `date` varchar(100) DEFAULT NULL,
+  PRIMARY KEY (`filim_id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+/*Data for the table `filim` */
+
+/*Table structure for table `login` */
+
+DROP TABLE IF EXISTS `login`;
+
+CREATE TABLE `login` (
+  `login_id` int(11) NOT NULL AUTO_INCREMENT,
+  `username` varchar(100) DEFAULT NULL,
+  `password` varchar(100) DEFAULT NULL,
+  `usertype` varchar(100) DEFAULT NULL,
+  PRIMARY KEY (`login_id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+/*Data for the table `login` */
+
+/*Table structure for table `payment` */
+
+DROP TABLE IF EXISTS `payment`;
+
+CREATE TABLE `payment` (
+  `payment_id` int(11) NOT NULL AUTO_INCREMENT,
+  `assigncrew_id` int(11) DEFAULT NULL,
+  `amount` varchar(100) DEFAULT NULL,
+  `date` varchar(100) DEFAULT NULL,
+  PRIMARY KEY (`payment_id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+/*Data for the table `payment` */
+
+/*Table structure for table `requirment` */
+
+DROP TABLE IF EXISTS `requirment`;
+
+CREATE TABLE `requirment` (
+  `requirment_id` int(11) NOT NULL AUTO_INCREMENT,
+  `assigncrew_id` int(11) DEFAULT NULL,
+  `requirment` varchar(100) DEFAULT NULL,
+  `date` varchar(100) DEFAULT NULL,
+  `status` varchar(100) DEFAULT NULL,
+  PRIMARY KEY (`requirment_id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+/*Data for the table `requirment` */
+
+/*Table structure for table `staff` */
+
+DROP TABLE IF EXISTS `staff`;
+
+CREATE TABLE `staff` (
+  `staff_id` int(11) NOT NULL AUTO_INCREMENT,
+  `login_id` int(11) DEFAULT NULL,
+  `fname` varchar(100) DEFAULT NULL,
+  `lname` varchar(100) DEFAULT NULL,
+  `place` varchar(100) DEFAULT NULL,
+  `phone` varchar(100) DEFAULT NULL,
+  `email` varchar(100) DEFAULT NULL,
+  PRIMARY KEY (`staff_id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+/*Data for the table `staff` */
+
+/*Table structure for table `updatework` */
+
+DROP TABLE IF EXISTS `updatework`;
+
+CREATE TABLE `updatework` (
+  `updatework_id` int(11) NOT NULL AUTO_INCREMENT,
+  `work_id` int(11) DEFAULT NULL,
+  `desc` varchar(100) DEFAULT NULL,
+  `date` varchar(100) DEFAULT NULL,
+  PRIMARY KEY (`updatework_id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+/*Data for the table `updatework` */
+
+/*Table structure for table `works` */
+
+DROP TABLE IF EXISTS `works`;
+
+CREATE TABLE `works` (
+  `work_id` int(11) NOT NULL AUTO_INCREMENT,
+  `assigncrew_id` int(11) DEFAULT NULL,
+  `work` varchar(100) DEFAULT NULL,
+  `details` varchar(100) DEFAULT NULL,
+  `date` varchar(100) DEFAULT NULL,
+  `status` varchar(100) DEFAULT NULL,
+  PRIMARY KEY (`work_id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+/*Data for the table `works` */
+
+/*!40101 SET SQL_MODE=@OLD_SQL_MODE */;
+/*!40014 SET FOREIGN_KEY_CHECKS=@OLD_FOREIGN_KEY_CHECKS */;
+/*!40014 SET UNIQUE_CHECKS=@OLD_UNIQUE_CHECKS */;
+/*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
