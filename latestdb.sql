b292a7cffcc0bef7bc08f097cf0a33dccbc0677a
sankar <sankusanku001@gmail.com>
2023-01-23 21:43:56 +0530

    database added

---
+++ latestdb.sql
@@ -0,0 +1,173 @@
+/*
+SQLyog Ultimate v11.11 (64 bit)
+MySQL - 5.5.5-10.4.27-MariaDB : Database - movie_production
+*********************************************************************
+*/
+
+/*!40101 SET NAMES utf8 */;
+
+/*!40101 SET SQL_MODE=''*/;
+
+/*!40014 SET @OLD_UNIQUE_CHECKS=@@UNIQUE_CHECKS, UNIQUE_CHECKS=0 */;
+/*!40014 SET @OLD_FOREIGN_KEY_CHECKS=@@FOREIGN_KEY_CHECKS, FOREIGN_KEY_CHECKS=0 */;
+/*!40101 SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='NO_AUTO_VALUE_ON_ZERO' */;
+/*!40111 SET @OLD_SQL_NOTES=@@SQL_NOTES, SQL_NOTES=0 */;
+CREATE DATABASE /*!32312 IF NOT EXISTS*/`movie_production` /*!40100 DEFAULT CHARACTER SET utf8mb4 COLLATE utf8mb4_general_ci */;
+
+USE `movie_production`;
+
+/*Table structure for table `assigncrew` */
+
+DROP TABLE IF EXISTS `assigncrew`;
+
+CREATE TABLE `assigncrew` (
+  `assigncrew_id` int(11) NOT NULL AUTO_INCREMENT,
+  `filim_id` int(11) DEFAULT NULL,
+  `crew_id` int(11) DEFAULT NULL,
+  `date` varchar(100) DEFAULT NULL,
+  `status` varchar(100) DEFAULT NULL,
+  PRIMARY KEY (`assigncrew_id`)
+) ENGINE=InnoDB AUTO_INCREMENT=3 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+/*Data for the table `assigncrew` */
+
+insert  into `assigncrew`(`assigncrew_id`,`filim_id`,`crew_id`,`date`,`status`) values (2,2,2,'2023-01-23','pending');
+
+/*Table structure for table `crew` */
+
+DROP TABLE IF EXISTS `crew`;
+
+CREATE TABLE `crew` (
+  `crew_id` int(11) NOT NULL AUTO_INCREMENT,
+  `login_id` int(11) DEFAULT NULL,
+  `fname` varchar(100) DEFAULT NULL,
+  `lname` varchar(100) DEFAULT NULL,
+  `place` varchar(100) DEFAULT NULL,
+  `phone` varchar(100) DEFAULT NULL,
+  `email` varchar(100) DEFAULT NULL,
+  PRIMARY KEY (`crew_id`)
+) ENGINE=InnoDB AUTO_INCREMENT=3 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+/*Data for the table `crew` */
+
+insert  into `crew`(`crew_id`,`login_id`,`fname`,`lname`,`place`,`phone`,`email`) values (2,5,'cheecy','piza','Kottayam','6238526459','admin@gmail.com');
+
+/*Table structure for table `filim` */
+
+DROP TABLE IF EXISTS `filim`;
+
+CREATE TABLE `filim` (
+  `filim_id` int(11) NOT NULL AUTO_INCREMENT,
+  `filim` varchar(100) DEFAULT NULL,
+  `details` varchar(100) DEFAULT NULL,
+  `desc` varchar(100) DEFAULT NULL,
+  `date` varchar(100) DEFAULT NULL,
+  PRIMARY KEY (`filim_id`)
+) ENGINE=InnoDB AUTO_INCREMENT=3 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+/*Data for the table `filim` */
+
+insert  into `filim`(`filim_id`,`filim`,`details`,`desc`,`date`) values (2,'anaconda','simple','rng','2022-12-28');
+
+/*Table structure for table `login` */
+
+DROP TABLE IF EXISTS `login`;
+
+CREATE TABLE `login` (
+  `login_id` int(11) NOT NULL AUTO_INCREMENT,
+  `username` varchar(100) DEFAULT NULL,
+  `password` varchar(100) DEFAULT NULL,
+  `usertype` varchar(100) DEFAULT NULL,
+  PRIMARY KEY (`login_id`)
+) ENGINE=InnoDB AUTO_INCREMENT=6 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+/*Data for the table `login` */
+
+insert  into `login`(`login_id`,`username`,`password`,`usertype`) values (1,'admin','admin','admin'),(3,'staff','staff','staff'),(5,'crew','crew','crew');
+
+/*Table structure for table `payment` */
+
+DROP TABLE IF EXISTS `payment`;
+
+CREATE TABLE `payment` (
+  `payment_id` int(11) NOT NULL AUTO_INCREMENT,
+  `assigncrew_id` int(11) DEFAULT NULL,
+  `amount` varchar(100) DEFAULT NULL,
+  `date` varchar(100) DEFAULT NULL,
+  PRIMARY KEY (`payment_id`)
+) ENGINE=InnoDB AUTO_INCREMENT=3 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+/*Data for the table `payment` */
+
+insert  into `payment`(`payment_id`,`assigncrew_id`,`amount`,`date`) values (2,2,'500','2023-01-23');
+
+/*Table structure for table `requirment` */
+
+DROP TABLE IF EXISTS `requirment`;
+
+CREATE TABLE `requirment` (
+  `requirment_id` int(11) NOT NULL AUTO_INCREMENT,
+  `assigncrew_id` int(11) DEFAULT NULL,
+  `requirment` varchar(100) DEFAULT NULL,
+  `date` varchar(100) DEFAULT NULL,
+  `status` varchar(100) DEFAULT NULL,
+  PRIMARY KEY (`requirment_id`)
+) ENGINE=InnoDB AUTO_INCREMENT=3 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+/*Data for the table `requirment` */
+
+insert  into `requirment`(`requirment_id`,`assigncrew_id`,`requirment`,`date`,`status`) values (1,2,'money','2023-01-23','Payment Completed'),(2,2,'power','2023-01-23','pending');
+
+/*Table structure for table `staff` */
+
+DROP TABLE IF EXISTS `staff`;
+
+CREATE TABLE `staff` (
+  `staff_id` int(11) NOT NULL AUTO_INCREMENT,
+  `login_id` int(11) DEFAULT NULL,
+  `fname` varchar(100) DEFAULT NULL,
+  `lname` varchar(100) DEFAULT NULL,
+  `place` varchar(100) DEFAULT NULL,
+  `phone` varchar(100) DEFAULT NULL,
+  `email` varchar(100) DEFAULT NULL,
+  PRIMARY KEY (`staff_id`)
+) ENGINE=InnoDB AUTO_INCREMENT=3 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+/*Data for the table `staff` */
+
+insert  into `staff`(`staff_id`,`login_id`,`fname`,`lname`,`place`,`phone`,`email`) values (2,3,'Aswin','Thampi','kochi','6238526459','aswin@gmail.com');
+
+/*Table structure for table `updatework` */
+
+DROP TABLE IF EXISTS `updatework`;
+
+CREATE TABLE `updatework` (
+  `updatework_id` int(11) NOT NULL AUTO_INCREMENT,
+  `work_id` int(11) DEFAULT NULL,
+  `desc` varchar(100) DEFAULT NULL,
+  `date` varchar(100) DEFAULT NULL,
+  PRIMARY KEY (`updatework_id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+/*Data for the table `updatework` */
+
+/*Table structure for table `works` */
+
+DROP TABLE IF EXISTS `works`;
+
+CREATE TABLE `works` (
+  `work_id` int(11) NOT NULL AUTO_INCREMENT,
+  `assigncrew_id` int(11) DEFAULT NULL,
+  `work` varchar(100) DEFAULT NULL,
+  `details` varchar(100) DEFAULT NULL,
+  `date` varchar(100) DEFAULT NULL,
+  `status` varchar(100) DEFAULT NULL,
+  PRIMARY KEY (`work_id`)
+) ENGINE=InnoDB AUTO_INCREMENT=3 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+/*Data for the table `works` */
+
+/*!40101 SET SQL_MODE=@OLD_SQL_MODE */;
+/*!40014 SET FOREIGN_KEY_CHECKS=@OLD_FOREIGN_KEY_CHECKS */;
+/*!40014 SET UNIQUE_CHECKS=@OLD_UNIQUE_CHECKS */;
+/*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
